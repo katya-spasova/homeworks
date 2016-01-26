c2366381d6803fa693f2154d95d24b0359f29dd9
Katya Spasova <katya.spasova11@gmail.com>
2016-01-26 22:55:15 +0200

    task 5

--- game_of_life/solution.go
+++ game_of_life/solution.go
@@ -1,1 +1,339 @@
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+// GameOfLife - holds the state of the game
+type GameOfLife struct {
+	generation int
+	board      map[int64](map[int64]bool)
+	rwMutex    sync.RWMutex
+	isEvolving bool
+}
+
+// GameOfLifeHandler - hold the game and multiplexer
+type GameOfLifeHandler struct {
+	mux        *http.ServeMux
+	gameOfLife GameOfLife
+}
+
+// Game of life implements Handler interface
+func (h *GameOfLifeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mux.ServeHTTP(w, r)
+}
+
+// Creates new GameOfLifeHandler
+func NewGameOfLifeHandler(startCells [][2]int64) *GameOfLifeHandler {
+	gameOfLife := GameOfLife{generation: 0,
+		board: make(map[int64]map[int64]bool), rwMutex: sync.RWMutex{},
+		isEvolving: false}
+	for i := 0; i < len(startCells); i++ {
+		gameOfLife.addCell(startCells[i][0], startCells[i][1])
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/cell/status/", gameOfLife.getCellStatus)
+	mux.HandleFunc("/generation/", gameOfLife.getGeneration)
+	mux.HandleFunc("/cells/", gameOfLife.addCells)
+	mux.HandleFunc("/generation/evolve/", gameOfLife.evolve)
+	mux.HandleFunc("/reset/", gameOfLife.reset)
+	mux.HandleFunc("/", wrongQuery)
+
+	gameOfLifeHandler := GameOfLifeHandler{mux: mux, gameOfLife: gameOfLife}
+
+	return &gameOfLifeHandler
+}
+
+// Add a living cell to the game board
+func (game *GameOfLife) addCell(x int64, y int64) {
+	addToBoard(game.board, x, y)
+}
+
+// Add (x, y) to a board
+func addToBoard(board map[int64]map[int64]bool, x int64, y int64) {
+	ym, ok := board[x]
+	if !ok {
+		ym = make(map[int64]bool)
+		board[x] = ym
+	}
+	ym[y] = true
+}
+
+// Check if cell (x, y) is alive
+func (game *GameOfLife) isAlive(x int64, y int64) bool {
+	ym, ok := game.board[x]
+	if !ok {
+		return false
+	}
+	alive, ok2 := ym[y]
+	if !ok2 {
+		return false
+	}
+	return alive
+}
+
+// Type used for creating json for /cell/status/ requests
+type Alive struct {
+	Alive bool `json:"alive"`
+}
+
+// Writes a response
+func message(w http.ResponseWriter, message []byte, code int) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	if message != nil {
+		w.Write(message)
+	}
+}
+
+// Responsible to answer to /cell/status/
+func (game *GameOfLife) getCellStatus(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
+	}
+	xStr := r.URL.Query().Get("x")
+	yStr := r.URL.Query().Get("y")
+	x, errX := strconv.ParseInt(xStr, 10, 64)
+	y, errY := strconv.ParseInt(yStr, 10, 64)
+	if errX != nil {
+		http.Error(w, errX.Error(), http.StatusBadRequest)
+	}
+	if errY != nil {
+		http.Error(w, errY.Error(), http.StatusBadRequest)
+	}
+	game.rwMutex.RLock()
+	alive, _ := json.Marshal(Alive{Alive: game.isAlive(x, y)})
+	game.rwMutex.RUnlock()
+
+	message(w, alive, http.StatusOK)
+}
+
+// Type used for creating json for /generation/ requests
+type Generation struct {
+	Generation int        `json:"generation"`
+	Living     [][2]int64 `json:"living"`
+}
+
+// Responsible to answer to /generation/ requests
+func (game *GameOfLife) getGeneration(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
+	}
+	game.rwMutex.RLock()
+	generation, _ := json.Marshal(Generation{Generation: game.generation, Living: game.getLiving()})
+	game.rwMutex.RUnlock()
+	message(w, generation, http.StatusOK)
+
+}
+
+// Returns all the living cells on the game board
+func (game *GameOfLife) getLiving() [][2]int64 {
+	living := make([][2]int64, 0)
+	for x, ym := range game.board {
+		for y, alive := range ym {
+			if alive {
+				s := [2]int64{x, y}
+				living = append(living, s)
+			}
+		}
+	}
+	return living
+}
+
+// Type used to hold the points received with /cells/ requests
+type Point struct {
+	X int64 `json:"x"`
+	Y int64 `json:"y"`
+}
+
+// Responsible to answer to /cells/ requests
+func (game *GameOfLife) addCells(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+	}
+
+	defer r.Body.Close()
+	bytes, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	s := make([]Point, 0)
+	err1 := json.Unmarshal(bytes, &s)
+
+	if err1 != nil {
+		http.Error(w, err1.Error(), http.StatusBadRequest)
+	}
+
+	game.rwMutex.Lock()
+	//ugly :(
+	for game.isEvolving {
+		game.rwMutex.Unlock()
+		time.Sleep(1 * time.Millisecond)
+		game.rwMutex.Lock()
+	}
+	for _, p := range s {
+		game.addCell(p.X, p.Y)
+	}
+	game.rwMutex.Unlock()
+
+	message(w, nil, http.StatusCreated)
+}
+
+// Responsible to answer to /generation/evolve/ requests
+func (game *GameOfLife) evolve(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+	}
+
+	// has to lock here for a little while
+	game.rwMutex.Lock()
+	//ugly :(
+	for game.isEvolving {
+		game.rwMutex.Unlock()
+		time.Sleep(1 * time.Millisecond)
+		game.rwMutex.Lock()
+	}
+	game.isEvolving = true
+	game.rwMutex.Unlock()
+
+	game.rwMutex.RLock()
+
+	newBoard := make(map[int64]map[int64]bool)
+
+	for x, ym := range game.board {
+		for y, alive := range ym {
+			if alive {
+				count := game.getLivingNeighbours(x, y)
+				if count == 2 || count == 3 {
+					addToBoard(newBoard, x, y)
+				}
+
+				//a dead cell with 3 alive neighbours is to be found only around living cells
+				//so check the neighbours if this cell
+				game.addBornCellsAround(newBoard, x, y)
+			}
+		}
+	}
+	game.rwMutex.RUnlock()
+
+	// it is unwise to allow reading at this point, so lock again
+	game.rwMutex.Lock()
+	game.generation += 1
+	game.board = newBoard
+	game.isEvolving = false
+	game.rwMutex.Unlock()
+
+	message(w, nil, http.StatusNoContent)
+}
+
+// Returns the number of living neighbours around a cell. Counts only to 4 to be more efficient
+func (game *GameOfLife) getLivingNeighbours(x int64, y int64) (count int) {
+	var min int64 = math.MinInt64
+	var max int64 = math.MaxInt64
+
+	var minX, minY, maxX, maxY int64
+
+	//clear corner cases
+	if x > min {
+		minX = x - 1
+	}
+	if x < max {
+		maxX = x + 1
+	}
+	if y > min {
+		minY = y - 1
+	}
+	if y < max {
+		maxY = y + 1
+	}
+
+	count = 0
+
+	for i := minX; i <= maxX; i++ {
+		for j := minY; j <= maxY; j++ {
+			if i == x && j == y {
+				continue
+			}
+			if game.isAlive(i, j) {
+				count += 1
+				// no reason to check for more than 4 alive neighbours since the cell is overcrowded
+				if count == 4 {
+					return count
+				}
+			}
+		}
+	}
+	return count
+}
+
+// Searches for places where cells have to be born and adds them to the new board
+func (game *GameOfLife) addBornCellsAround(newBoard map[int64]map[int64]bool, x int64, y int64) {
+	var min int64 = math.MinInt64
+	var max int64 = math.MaxInt64
+
+	var minX, minY, maxX, maxY int64
+
+	//clear corner cases
+	if x > min {
+		minX = x - 1
+	}
+	if x < max {
+		maxX = x + 1
+	}
+	if y > min {
+		minY = y - 1
+	}
+	if y < max {
+		maxY = y + 1
+	}
+
+	for i := minX; i <= maxX; i++ {
+		for j := minY; j <= maxY; j++ {
+			if i == x && j == y {
+				// skip the center of the search
+				continue
+			}
+			if !game.isAlive(i, j) {
+				// dead cell found - count its neighbours
+				count := game.getLivingNeighbours(i, j)
+				if count == 3 {
+					addToBoard(newBoard, i, j)
+				}
+			}
+		}
+	}
+}
+
+// Responsible to answer to /reset/ requests
+func (game *GameOfLife) reset(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+	}
+	game.rwMutex.Lock()
+	//ugly :(
+	for game.isEvolving {
+		game.rwMutex.Unlock()
+		time.Sleep(1 * time.Millisecond)
+		game.rwMutex.Lock()
+	}
+	game.generation = 0
+	game.board = make(map[int64]map[int64]bool)
+	game.rwMutex.Unlock()
+
+	message(w, nil, http.StatusNoContent)
+}
+
+// Used to return status code 400 to wrong requests
+func wrongQuery(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "", http.StatusBadRequest)
+}
--- game_of_life/solution_test.go
+++ game_of_life/solution_test.go
@@ -1,1 +1,370 @@
 package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCreatingBoardWithSeed(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{3, 4},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+	testTable := []struct {
+		alive bool
+		x, y  int64
+	}{
+		{x: 0, y: 0, alive: true},
+		{x: 1, y: 1, alive: true},
+		{x: 3, y: 4, alive: true},
+		{x: -2, y: 5, alive: true},
+		{x: -19023482123, y: 5, alive: true},
+		{x: 0, y: -1, alive: false},
+		{x: -100, y: 100, alive: false},
+		{x: 100, y: 100, alive: false},
+		{x: 55, y: 93, alive: false},
+	}
+
+	for _, testCase := range testTable {
+		path := fmt.Sprintf("/cell/status/?x=%d&y=%d", testCase.x, testCase.y)
+		cellURL := buildUrl(testSrv.URL, path)
+
+		resp, err := http.Get(cellURL)
+
+		if err != nil {
+			t.Errorf("Error getting empty board: %s", err)
+		}
+
+		defer resp.Body.Close()
+
+		cellStatus := &struct {
+			Alive bool `json:"alive"`
+		}{}
+
+		respBytes, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil && err != io.EOF {
+			t.Errorf("Error reading from response: %s", err)
+		}
+
+		fmt.Println("Response body " + string(respBytes[:]))
+
+		if err := json.Unmarshal(respBytes, cellStatus); err != nil {
+			t.Errorf("Error decoding json: %s", err)
+		}
+
+		if testCase.alive != cellStatus.Alive {
+			t.Errorf("Expected alive %t for (%d, %d) but it was %t. "+
+				"JSON: %s", testCase.alive, testCase.x, testCase.y, cellStatus.Alive,
+				string(respBytes))
+		}
+	}
+
+}
+
+func TestGetLiving(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{3, 4},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+	url := buildUrl(testSrv.URL, "/generation/")
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+
+	//todo: check the value with some code
+	fmt.Println(string(respBytes[:]))
+}
+
+func TestReset(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{3, 4},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+	url := buildUrl(testSrv.URL, "/generation/")
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+
+	//todo: check the value with some code
+	fmt.Println(string(respBytes[:]))
+
+	url1 := buildUrl(testSrv.URL, "/reset/")
+	resp1, err1 := http.Post(url1, "text/plain", nil)
+	if err1 != nil {
+		t.Fatal(err1.Error())
+	}
+
+	defer resp1.Body.Close()
+	if resp1.StatusCode != http.StatusNoContent {
+		t.Errorf("Expected status 204 but found %d", resp1.StatusCode)
+	}
+
+	url2 := buildUrl(testSrv.URL, "/generation/")
+	resp2, err2 := http.Get(url2)
+	if err2 != nil {
+		t.Fatal(err2.Error())
+	}
+
+	defer resp2.Body.Close()
+
+	respBytes2, err2 := ioutil.ReadAll(resp2.Body)
+
+	//todo: check the value with some code
+	fmt.Println(string(respBytes2[:]))
+}
+
+func TestAddCell(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{3, 4},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+	url1 := buildUrl(testSrv.URL, "/cells/")
+	var data = []byte(`[{"x": 42, "y": 43}, {"x": -5, "y": 10}]`)
+
+	resp1, err1 := http.Post(url1, "application/json", bytes.NewBuffer(data))
+	if err1 != nil {
+		t.Fatal(err1.Error())
+	}
+
+	defer resp1.Body.Close()
+
+	if resp1.StatusCode != http.StatusCreated {
+		t.Errorf("Expected status 201 but found %d", resp1.StatusCode)
+	}
+
+	url2 := buildUrl(testSrv.URL, "/generation/")
+	resp2, err2 := http.Get(url2)
+	if err2 != nil {
+		t.Fatal(err2.Error())
+	}
+
+	defer resp2.Body.Close()
+
+	respBytes2, err2 := ioutil.ReadAll(resp2.Body)
+
+	//todo: check the value with some code
+	fmt.Println(string(respBytes2[:]))
+}
+
+func TestAddCellDupl(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{3, 4},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+	url1 := buildUrl(testSrv.URL, "/cells/")
+	var data = []byte(`[{"x": 42, "y": 43}, {"x": 3, "y": 4}]`)
+
+	resp1, err1 := http.Post(url1, "application/json", bytes.NewBuffer(data))
+	if err1 != nil {
+		t.Fatal(err1.Error())
+	}
+
+	defer resp1.Body.Close()
+
+	if resp1.StatusCode != http.StatusCreated {
+		t.Errorf("Expected status 201 but found %d", resp1.StatusCode)
+	}
+
+	url2 := buildUrl(testSrv.URL, "/generation/")
+	resp2, err2 := http.Get(url2)
+	if err2 != nil {
+		t.Fatal(err2.Error())
+	}
+
+	defer resp2.Body.Close()
+
+	respBytes2, err2 := ioutil.ReadAll(resp2.Body)
+
+	//todo: check the value with some code
+	fmt.Println(string(respBytes2[:]))
+}
+
+func TestNeighbours(t *testing.T) {
+	gameOfLife := GameOfLife{generation: 0, board: make(map[int64]map[int64]bool), rwMutex: sync.RWMutex{}}
+	gameOfLife.addCell(1, 2)
+	gameOfLife.addCell(2, 3)
+
+	count := gameOfLife.getLivingNeighbours(1, 2)
+	if count != 1 {
+		t.Errorf("EXpected count is one, found %d", count)
+	}
+
+	gameOfLife.addCell(math.MinInt64, math.MinInt64)
+	gameOfLife.addCell(math.MaxInt64, math.MaxInt64)
+
+	count = gameOfLife.getLivingNeighbours(math.MinInt64, math.MinInt64)
+	if count != 0 {
+		t.Errorf("EXpected count is 0, found %d", count)
+	}
+
+	count = gameOfLife.getLivingNeighbours(math.MaxInt64, math.MaxInt64)
+	if count != 0 {
+		t.Errorf("Expected count is 0, found %d", count)
+	}
+}
+
+func TestEvolve(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{1, 2},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+	url := buildUrl(testSrv.URL, "/generation/evolve/")
+	resp, err := http.Post(url, "text/plain", nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent {
+		t.Errorf("Expected status 204 but found %d", resp.StatusCode)
+	}
+
+	url1 := buildUrl(testSrv.URL, "/generation/")
+	resp1, err1 := http.Get(url1)
+	if err1 != nil {
+		t.Fatal(err1.Error())
+	}
+
+	defer resp1.Body.Close()
+
+	respBytes, _ := ioutil.ReadAll(resp1.Body)
+
+	//todo: check the value with some code
+	fmt.Println(string(respBytes[:]))
+}
+
+func TestAsyncEvolve(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{1, 2},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+
+	url := buildUrl(testSrv.URL, "/generation/evolve/")
+
+	for i := 0; i < 100; i++ {
+		go func(url string) {
+			resp, err := http.Post(url, "text/plain", nil)
+
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusNoContent {
+				t.Errorf("Expected status 204 but found %d", resp.StatusCode)
+			}
+		}(url)
+	}
+
+	url1 := buildUrl(testSrv.URL, "/cells/")
+	var data = []byte(`[{"x": 42, "y": 43}, {"x": -5, "y": 10}]`)
+
+	for j := 0; j < 100; j++ {
+		go func(url1 string) {
+			resp1, err1 := http.Post(url1, "application/json", bytes.NewBuffer(data))
+			if err1 != nil {
+				t.Fatal(err1.Error())
+			}
+
+			defer resp1.Body.Close()
+
+			if resp1.StatusCode != http.StatusCreated {
+				t.Errorf("Expected status 201 but found %d", resp1.StatusCode)
+			}
+		}(url1)
+	}
+	time.Sleep(10 * time.Second)
+}
+
+func TestBadQuery(t *testing.T) {
+	testSrv := setUpServer([][2]int64{
+		{0, 0},
+		{1, 1},
+		{1, 2},
+		{-2, 5},
+		{-19023482123, 5},
+	})
+	defer testSrv.Close()
+
+	url := buildUrl(testSrv.URL, "/wrong")
+	resp, err := http.Post(url, "text/plain", nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status 400 but found %d", resp.StatusCode)
+	}
+}
+
+/* Utility functions */
+
+func buildUrl(baseUrl, path string) string {
+	return fmt.Sprintf("%s%s", baseUrl, path)
+}
+
+// Users of this function are resposible for calling Close() on the returned server.
+// Failure to do so will result in leaked resources.
+func setUpServer(cells [][2]int64) *httptest.Server {
+	gofh := NewGameOfLifeHandler(cells)
+	return httptest.NewServer(gofh)
+}
